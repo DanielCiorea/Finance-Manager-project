b233da72fa5bb3e5fa8c0332878ca459ce3ead42
Daniel Ciorea <58229367+DanielCiorea@users.noreply.github.com>
2020-05-04 10:00:33 +0300

    update

--- web/src/EntryList.js
+++ web/src/EntryList.js
@@ -5,36 +5,104 @@ export default class EntryList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      descriptionValue: "",
-      amountValue: "",
+      budget: props.budget,
+      description: "",
+      amount: "",
       entries: [],
+      filter: "ALL",
     };
   }
 
-  // Add modify functionality
   modifyEntry = async (id, description, amount) => {
     await modifyEntry(id, description, amount);
     const entries = await listEntries();
     this.setState({ entries });
   };
 
+  deleteEntry = async (id) => {
+    await deleteEntry(id);
+    const entries = await listEntries();
+    this.setState({ entries });
+  };
+
+  setFilterToAll = () => {
+    this.setState({ filter: "ALL" });
+  };
+
+  setFilterToIncomes = () => {
+    this.setState({ filter: "INCOMES" });
+  };
+
+  setFilterToExpenses = () => {
+    this.setState({ filter: "EXPENSES" });
+  };
+
+  filterEntries = (entries, filter) =>
+    filter === "ALL"
+      ? entries
+      : filter === "INCOMES"
+      ? entries.filter((entry) => entry.amount >= 0)
+      : entries.filter((entry) => entry.amount < 0);
+
   async componentDidMount() {
     const entries = await listEntries();
     this.setState({ entries });
   }
 
+  onInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onInputKeyPress = async (event) => {
+    if (event.nativeEvent.keyCode === 13) {
+      await createEntry(this.state.description, this.state.amount);
+      const entries = await listEntries();
+      this.setState({ entries, description: "", amount: "" });
+    }
+  };
+
   render() {
+    const filteredEntries = this.filterEntries(
+      this.state.entries,
+      this.state.filter
+    );
+
     return (
-      <div>
-        <input className="input" value={this.state.descriptionValue} placeholder={"Description"} />
-        <input className="input" value={this.state.amountValue} placeholder={"Amount"} />
-        {this.state.entries.map((entry) => (
+      <div className="container">
+        Description:
+        <input
+          className="input"
+          name="description"
+          value={this.state.description}
+          onChange={this.onInputChange}
+          onKeyPress={this.onInputKeyPress}
+        />
+        Amount:
+        <input
+          type="number"
+          className="input"
+          name="amount"
+          value={this.state.amount}
+          onChange={this.onInputChange}
+          onKeyPress={this.onInputKeyPress}
+        />
+        {filteredEntries.map((entry) => (
           <label key={entry.id} className="entry">
             <button>edit</button>
             {entry.description} | ${entry.amount}
-            <button>delete</button>
+            <button
+              className="delButton"
+              onClick={() => this.deleteEntry(entry.id)}
+            >
+              🗑
+            </button>
           </label>
         ))}
+        <div>
+          <button onClick={this.setFilterToAll}>All</button>
+          <button onClick={this.setFilterToIncomes}>Incomes</button>
+          <button onClick={this.setFilterToExpenses}>Expenses</button>
+        </div>
       </div>
     );
   }
